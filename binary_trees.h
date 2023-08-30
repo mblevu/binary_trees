daa80c8cf44438cf6f6aa8a7d244b7b62b7bed30
kintokeanu <levu@kali>
2023-08-30 23:30:12 +0300

    counting nodes with atleast 1 child

--- binary_trees.h
+++ binary_trees.h
@@ -56,6 +56,7 @@ size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 /*counts the leaves in a binary tree*/
 size_t binary_tree_leaves(const binary_tree_t *tree);
-
+/*counts the nodes with at least 1 child in a binary tree*/
+size_t binary_tree_nodes(const binary_tree_t *tree);
 
 #endif
