0ad53e66fda3a7a01374cb37e85e7dc5494a2ba0
kintokeanu <levu@kali>
2023-08-30 21:26:33 +0300

    traversing node inorder

--- binary_trees.h
+++ binary_trees.h
@@ -44,6 +44,8 @@ int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
 /*goes through a binary tree using pre-order traversal*/
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+/*goes through a binary tree using in-order traversal*/
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 
 
 #endif
