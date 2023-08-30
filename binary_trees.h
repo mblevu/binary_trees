c9f831195cf864df4bea3533f5d8bf526be59955
kintokeanu <levu@kali>
2023-08-30 22:34:53 +0300

    getting tree depth

--- binary_trees.h
+++ binary_trees.h
@@ -50,6 +50,8 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 /*measures the height of a binary tree*/
 size_t binary_tree_height(const binary_tree_t *tree);
+/*measures the depth of a node in a binary tree*/
+size_t binary_tree_depth(const binary_tree_t *tree);
 
 
 #endif
