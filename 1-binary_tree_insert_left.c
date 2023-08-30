1c87c18869e88918755ec3bc44492d0240039a1b
kintokeanu <levu@kali>
2023-08-30 19:51:29 +0300

    inserting  node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -4,6 +4,7 @@
  * binary_tree_insert_left - inserts a node as the left child of another node
  * @parent:pointer to parent node
  * @value: value to put in the new node
+ * Return:  new node
 */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -4,6 +4,7 @@
  * binary_tree_insert_right- inserts a node as the right-child of another node
  * @parent:pointer to parent node
  * @value: value to put in the new node
+ * Return: new node
  *
 */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
